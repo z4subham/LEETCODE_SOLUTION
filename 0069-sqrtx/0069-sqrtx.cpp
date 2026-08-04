3ef9f04fd8aba0316909ad97f7e58a6e55eec99b
subham kumar <kumarsubham40420@gmail.com>
2026-08-05 01:41:42 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.04%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -5,13 +5,21 @@ public:
         if(x == 0){
             return 0 ;
         }
+        long long low = 1 ;
+        long long high = x ;
         long long ans = 1 ;
-        for(long long i=1 ; i*i <= x ; i++){
-            if(i*i <= x){
-                ans = i ; 
+        while(low <= high){
+            long long mid = (low + high) / 2 ;
+
+            if(mid * mid == x){
+                return mid ;
+            }
+            else if(mid * mid > x){
+                high = mid - 1 ;
             }
             else{
-                break ;
+                ans = mid ;
+                low = mid + 1 ;
             }
         }
         return ans ;
