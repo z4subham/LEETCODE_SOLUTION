e7db81193c7a65dcfe6073d33e2cbb6082bc0a74
subham kumar <kumarsubham40420@gmail.com>
2026-09-16 18:28:48 +0530

    Time: 0 ms (100%), Space: 9 MB (33.09%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         
-        int n = s.length() ;
-        int ctr = 0 ; 
+        int n = s.length() - 1;
+        int i = n ;
+        int ctr = 0 ;
 
-        //skip the empty spaces :- 
-        int i = n-1 ;
-        while(i>=0 && s[i] == ' '){
+       // for removing the spaces :- 
+        while (i >= 0 && s[i] == ' '){
             i-- ;
         }
-        //counting the character :- 
-        while(i>= 0 && s[i] !=  ' '){
+
+        while(i>=0 && s[i] != ' '){
             ctr++ ;
             i-- ;
-        }
+        } 
+
         return ctr ;
     }
 };
