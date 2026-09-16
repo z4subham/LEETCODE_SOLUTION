475a5cd6f9eac902f0868d4a101f4d165d3a52b0
subham kumar <kumarsubham40420@gmail.com>
2026-09-16 18:07:29 +0530

    Time: 0 ms (100%), Space: 9.9 MB (25.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
 
-        if( s.length() != t.length()){
+        if(s.length() != t.length()){
             return false ;
-        }
-        unordered_map<char ,int > mpp ;
-
-        //map of string s is created :- 
+        } 
+        
+        vector<int> hash_1(26,0) ;
         for(int i=0 ; i<s.length() ; i++){
-            mpp[s[i]]++ ;
+            hash_1[s[i] - 'a']++ ;
+        }
+
+        vector<int> hash_2(26 , 0) ;
+        for(int i=0 ; i< t.length() ; i++){
+            hash_2[t[i] - 'a']++ ;
         } 
 
-        //iterate in map :- 
-        for(int i=0 ; i<t.length() ; i++){
-            mpp[t[i]]-- ;
-        }
-
-        for(auto it : mpp){
-            if(it.second != 0){
-                return false ;
+        //iterate in hash arr :- 
+        for(int i=0 ; i<hash_1.size() ; i++){
+            if(hash_1[i] != hash_2[i]){
+                    return false ;
             }
         }
         return true ;
